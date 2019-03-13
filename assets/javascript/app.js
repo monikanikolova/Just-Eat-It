8252fa9cdb9347c7d1b72e130d525837b08a6c00
Qbisst <ismaelaperez@outlook.com>
2019-03-12 22:16:51 -0400

    testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,11 @@
 AOS.init();
+//on click event for "See More" button//
+$("#kebobs").on("click", function(event){
+    //Opens the results page//
+    window.open("results.html");
+    
+ 
 
-//Test comment//
+
+
+})
---
+++ assets/javascript/config.js
@@ -0,0 +1,4 @@
+var config = {
+    app_id="05e0ac53"
+    app_key="4481f6a99bd5786454eac4b4f6a3e005"
+}
